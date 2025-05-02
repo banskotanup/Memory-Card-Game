7eabc2c4669a586623ce69862e67d5c417fb2d5a
banskotanup <baskota.anup11@gmail.com>
2025-05-02 21:29:48 +0545

    style: reduce AOS delay for difficulty selection buttons for improved responsiveness

--- src/components/DifficultySelector.jsx
+++ src/components/DifficultySelector.jsx
@@ -3,9 +3,9 @@ export default function DifficultySelector({ setDifficulty }) {
         <div className="difficulty-selector">
             <label data-aos="fade-in" data-aos-easing="ease-in" ata-aos-delay="500">Select difficulty:</label>
             <div>
-            <button onClick={()=> setDifficulty(6)} className="easy" data-aos="zoom-out-down" data-aos-delay="500">Easy</button>
-            <button onClick={()=> setDifficulty(12)} className="medium" data-aos="zoom-out-up" data-aos-delay="500">Medium</button>
-            <button onClick={()=> setDifficulty(18)} className="hard" data-aos="zoom-out-left" data-aos-delay="500">Hard</button>
+            <button onClick={()=> setDifficulty(6)} className="easy" data-aos="zoom-out-down" data-aos-delay="100">Easy</button>
+            <button onClick={()=> setDifficulty(12)} className="medium" data-aos="zoom-out-up" data-aos-delay="100">Medium</button>
+            <button onClick={()=> setDifficulty(18)} className="hard" data-aos="zoom-out-left" data-aos-delay="100">Hard</button>
             </div>
         </div>
     );
